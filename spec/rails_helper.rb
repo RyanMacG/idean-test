a8ba4000d013940f9637363337f36a438a5f403a
Ryan MacGillivray <rmacg89@gmail.com>
2020-02-04 08:50:35 +0000

    Add database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,4 +23,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
